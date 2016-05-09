24097da72db659e38fed1c6b159aceb82819945c
yukipyon <wolfvbear@gmail.com>
2016-05-09 18:12:17 +0000

    New book tables.

---
+++ yukipyon/_scripts/books.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+racket gen-book-table.rkt books-programming > ../_includes/books-programming.tab
+racket gen-book-table.rkt books-security > ../_includes/books-security.tab
+racket gen-book-table.rkt books-other > ../_includes/books-other.tab
