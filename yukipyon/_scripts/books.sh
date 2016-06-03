8d80b1e84fae6b2c716de3a62d3ba22bf48d8b38
yukipyon <wolfvbear@gmail.com>
2016-06-03 18:09:26 +0000

    Remove old yukipyon directory.

--- yukipyon/_scripts/books.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-racket gen-book-table.rkt books-programming > ../_includes/books-programming.tab
-racket gen-book-table.rkt books-security > ../_includes/books-security.tab
-racket gen-book-table.rkt books-other > ../_includes/books-other.tab
